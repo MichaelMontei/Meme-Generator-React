7086f2945a5979ac2ba8c200f7c93e19378663a1
MichaelMontei <code-me@hotmail.com>
2022-09-12 10:38:50 +0200

    setup for new small project started, added Navbar component, added some styling and images for the Navbar.

---
+++ meme/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src="../images/png-transparent-rage-comic-internet-meme-trollface-internet-troll-meme-face-white-hand-photography.png"
+                className="header--image"
+            />
+            <h2 className="header--title">Header component</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
