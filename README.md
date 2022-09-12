b5bf851f3b80c72887a8a0de4d3a88deb3790b89
MichaelMontei <code-me@hotmail.com>
2022-09-12 10:44:59 +0200

    picture not working

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Meme-Generator-React
 Learning more React
+
+### Lets make some Memes  aaight!
+
+First lets start by making a 
--- meme/src/components/Header.js
+++ meme/src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img
-                src="../images/png-transparent-rage-comic-internet-meme-trollface-internet-troll-meme-face-white-hand-photography.png"
+                src="../images/troll-face.png"
                 className="header--image"
             />
             <h2 className="header--title">Header component</h2>
